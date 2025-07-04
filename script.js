1e4c9fd2f8439f278a3006aee517f23cfbb02a2a
Ogochukwu Emeh <emehaogochukwu@gmail.com>
2025-07-04 05:37:21 -0700

    Add sign functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+function divide(num1, num2) {
+    return num1 / num2;
+}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+
+
+console.log(add(5,7));
+console.log(subtract(8,3));
+console.log(divide(16,4));
+console.log(multiply(10,6));
